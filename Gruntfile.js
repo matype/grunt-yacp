466cb39375b7c1f3c7b0a76addc7eec2f8bdf23a
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-04 16:20:14 +0900

    delete comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 /*
  * grunt-yacp
  *
- *
  * Copyright (c) 2014 Masaaki Morishita
  * Licensed under the MIT license.
  */
@@ -9,10 +8,8 @@
 'use strict';
 
 module.exports = function (grunt) {
-  // load all npm grunt tasks
   require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
   grunt.initConfig({
     jshint: {
       all: [
@@ -25,12 +22,10 @@ module.exports = function (grunt) {
       }
     },
 
-    // Before generating any new files, remove any previously-created files.
     clean: {
       tests: ['tmp']
     },
 
-    // Configuration to be run (and then tested).
     yacp: {
       compile: {
         files: {
@@ -42,14 +37,8 @@ module.exports = function (grunt) {
 
   });
 
-  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
   grunt.registerTask('test', ['clean', 'yacp']);
-
-  // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- tasks/yacp.js
+++ tasks/yacp.js
@@ -1,7 +1,6 @@
 /*
  * grunt-yacp
  *
- *
  * Copyright (c) 2014 Masaaki Morishita
  * Licensed under the MIT license.
  */
@@ -14,17 +13,13 @@ module.exports = function (grunt) {
 
   grunt.registerMultiTask('yacp', 'Compile YACP (Yet Another CSS Preprocessor)', function () {
 
-    // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options ({
       punctuation: '.',
       separator: '\n'
     });
 
-    // Iterate over all specified file groups.
     this.files.forEach(function (file) {
-      // Concat specified files.
       var src = file.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
         if (!grunt.file.exists(filepath)) {
           grunt.log.warn('Source file "' + filepath + '" not found.');
           return false;
@@ -32,16 +27,13 @@ module.exports = function (grunt) {
           return true;
         }
       }).map(function (filepath) {
-        // Read file source.
         var yacpFile = grunt.file.read(filepath);
         var yacp = new Yacp(yacpFile);
         return yacp.toString();
       }).join(grunt.util.normalizelf(options.separator));
 
-      // Write the destination file.
       grunt.file.write(file.dest, src);
 
-      // Print a success message.
       grunt.log.writeln('File "' + file.dest + '" created.');
     });
   });
